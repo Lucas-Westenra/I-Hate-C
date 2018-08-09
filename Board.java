0576e1db92b8a4313ced0b96c6efbb6e98707219
Lucas-Westenra <41603083+Lucas-Westenra@users.noreply.github.com>
2018-08-09 17:03:11 +1200

    doSuggestion() moves players

--- Board.java
+++ Board.java
@@ -12,6 +12,7 @@ public class Board{
   private Tile[][] tiles = new Tile[24][25];
   private List<Weapon> weapons;
   private List<Player> players;
+  private List<Player> playing;
   private List<Door> doors;
   private List<Card> allCards;
   
@@ -23,7 +24,7 @@ public class Board{
 
 
   public Board(Tile[][] tiles, List<Player> players, List<Weapon> weapons,
-		  List<Card> murderCards, List<Door> doors, List<Card> allCards)
+		  List<Card> murderCards, List<Door> doors, List<Card> allCards, List<Player> playing)
   {
 	this.allCards = allCards;
     this.playersTurn = players.get((int)Math.random()*players.size());
@@ -36,6 +37,7 @@ public class Board{
     this.doors = doors;
     this.weapons = weapons;
     this.players = players;
+    this.playing = playing;
   }
 
   public boolean setPlayersTurn(Player aPlayersTurn)
@@ -195,6 +197,21 @@ public class Board{
   		//return (int)(Math.random()*10+2);
   	}
   	
+  	public Tile checkDir(Tile current) {
+  		int x = current.getXPos();
+  		int y = current.getYPos();
+  		String name = current.getName();
+  		if(y>0)
+  			if(tiles[x][y-1].getName().equals(name)) return tiles[x][y-1];
+  		if(x<23)
+  			if(tiles[x+1][y].getName().equals(name)) return tiles[x+1][y];
+  		if(y<24)
+  			if(tiles[x][y+1].getName().equals(name)) return tiles[x][y+1];
+  		if(x>0)
+  			if(tiles[x-1][y].getName().equals(name)) return tiles[x-1][y];
+  		return null;
+  	}
+  	
   	private void takeTurn(Player player) {
   		int moves = rollDice();
   		String tileName = "Walkway";
@@ -286,7 +303,7 @@ public class Board{
   		for(Card handCard: player.getHand()) { 
   			System.out.println("\t"+handCard.getType()+": "+handCard.getName());
   			for(int i=cardList.size()-1; i>0; i--) 
-  				if(cardList.get(i).getName().equals(handCard.getName())) 
+  				if(cardList.get(i).getName().equals(handCard.getName()) && !cardList.get(i).getType().equals("Room")) 
   					cardList.remove(i);
   		}
   		for(Card c: cardList) {
@@ -300,7 +317,20 @@ public class Board{
   				guessedCards.add(room);
   		guessedCards.add(getGuess(playerCards, "character"));
   		guessedCards.add(getGuess(weaponCards, "weapon"));
-  		for(int i=0;i<3;i++) System.out.println(guessedCards.get(i).getName());
+  		for(Player p: players) {
+  			if(p.getName().equals(guessedCards.get(1).getName())) {
+	  			p.getPosition().setPlayer(null);
+				p.setPosition(checkDir(player.getPosition()));
+				p.getPosition().setPlayer(p);
+  			}
+  		}
+  		for(Player p: playing) {
+  			for(Card c: guessedCards) {
+  				if(p.getHand().contains(c)) {
+  					
+  				}
+  			}
+  		}
   	}
   	
   	private void doAccusation(Player player) {
@@ -472,31 +502,30 @@ public class Board{
 		  players.get(selection-1).setPlayer(i);
 	  } 
 	  
-	  Player[] playing = new Player[nplayers];
-	  int count = 0;
-	  for(int p=0; p<players.size()-1; p++) { if(players.get(p).isPlaying()) playing[count++] = players.get(p); }
+	  List<Player> playing = new ArrayList<Player>();
+	  for(int p=0; p<players.size()-1; p++) { if(players.get(p).isPlaying()) playing.add(players.get(p)); }
 	  int nextPlayer = (int)Math.random()*nplayers+1;
 	  while(!cards.isEmpty()) {
 		  if(nextPlayer >= nplayers) nextPlayer = 1;
 		  int randomCard = (int)(Math.random()*(cards.size()-1));
-		  playing[nextPlayer-1].addToHand(cards.get(randomCard));
-		  cards.get(randomCard).setPlayer(playing[nextPlayer]);
+		  playing.get(nextPlayer-1).addToHand(cards.get(randomCard));
+		  cards.get(randomCard).setPlayer(playing.get(nextPlayer-1));
 		  cards.remove(randomCard);
 		  nextPlayer++;
 	  }
 	  
-	  Board board = new Board(tiles, players, weapons, murderCards, doors, allCards);
+	  Board board = new Board(tiles, players, weapons, murderCards, doors, allCards, playing);
 	  board.drawTiles();
 	  
-	  int playerTurn = 0;
+	  //int playerTurn = 0;
 	  while(!gameOver) {
-		  for(Player p: players) {
-			  if(p.getPlayer() == playerTurn) {
+		  for(Player p: playing) {
+			  //if(p.getPlayer() == playerTurn) {
 				  board.takeTurn(p);
-				  playerTurn++;
-			  }
+				  //playerTurn++;
+			  //}
 		  }
-		  playerTurn=0;
+		  //playerTurn=0;
 	  }
 	  
 	  
